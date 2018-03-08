ee928353f3f2c664a346e005158a2db0415e42cf
jason kim <htkim40@gmail.com>
2018-03-08 01:56:04 +0000

    Restructured read/write Bytes to be more general purpose,unit test now includes writing the gyro configuration registers

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -4,10 +4,33 @@
 #include <sys/ioctl.h> 
 #include <linux/i2c-dev.h>
  
+#define GCONFIG  0b00001000
+
 #define RBIT 0x80
 #define MPUADDR 0x68 // if AD0 is set to 0, then 0x68, else if set to high then 0x69
-//#define RMPUADDR 0xE8
 #define WHOAMI 0x75
+
+#define SELF_TEST_X_GYRO 0x00
+#define SELF_TEST_Y_GYRO 0x01
+#define SELF_TEST_Z_GYRO 0x02
+#define SELF_TEST_X_ACCEL 0x0D
+#define SELF_TEST_Y_ACCEL 0x0E
+#define SELF_TEST_Z_ACCEL 0x0F
+#define XG_OFFSET_H 0x13
+#define XG_OFFSET_L 0x14
+#define YG_OFFSET_H 0x15
+#define YG_OFFSET_L 0x16
+#define ZG_OFFSET_H 0x17
+#define ZG_OFFSET_L 0x18
+#define SMPLRT_DIV 0x19
+#define CONFIG 0x1A
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C 
+#define ACCEL_CONFIG_2 0x1D
+#define LP_ACCEL_ODR 0x1E
+#define WOM_THR 0x1F
+#define FIFO_EN 0x23
+#define I2C_MST_CTRL 0x24
 
 class mpu9250{
 	public: 
@@ -15,14 +38,17 @@ class mpu9250{
 		mpu9250();
 		~mpu9250();
 		void readWHOAMI();
-		void readBytes(int regAddr, int rlength);
+		void readCONFIG();
+		void writeCONFIG();
+		void readGCONFIG();
+		void writeGCONFIG(unsigned char gconfig);
 	private: 
 		char *filename = (char*)"/dev/i2c-1"; //device number may vary : "/dev/i2c-n"
 		const int addr = 0x68; // the i2c adapter address 	
 		int i2c_fd; //i2cdev file address 
 
-		void writeMPU(int wlength); 
-
+		void writeBytes(unsigned char* data, int wlength);
+		void readBytes(unsigned char regAddr, int rlength);
 };
 
 mpu9250::mpu9250(){
@@ -37,14 +63,14 @@ mpu9250::~mpu9250(){
 	close(i2c_fd);
 }
 
-void mpu9250::readBytes(int regAddr, int rlength){
+void mpu9250::readBytes(unsigned char regAddr, int rlength){
 
 	if(ioctl(i2c_fd, I2C_SLAVE, MPUADDR)<0){
 		std::cout<<"Failed to aquire bus access and/or talk to i2c slave."<<std::endl; 
 		exit(0);
 	}
-	wbuffer[0] = regAddr;
-	if(write(i2c_fd, wbuffer, 1)<1){
+
+	if(write(i2c_fd,&regAddr, 1)<1){
 		std::cout<<"Failed to write to the i2c bus."<<std::endl; 
 		return;
 	}
@@ -59,38 +85,54 @@ void mpu9250::readBytes(int regAddr, int rlength){
 	}
 }
 
-void mpu9250::writeMPU(int wlength) {
-	
-	if(ioctl(i2c_fd, I2C_SLAVE,MPUADDR)<0){
+void mpu9250::writeBytes(unsigned char* data, int wlength) {
+	if(ioctl(i2c_fd, I2C_SLAVE, MPUADDR)<0){
 		std::cout<<"Failed to aquire bus access and/or talk to i2c slave."<<std::endl; 
 		exit(0);
 	}
 
-	if(write(i2c_fd, wbuffer, wlength)!= wlength){
+	if(write(i2c_fd, data, wlength)!= wlength){
 		std::cout<<"Failed to write to the i2c bus."<<std::endl; 
 		return;
 	}
 }
 
 void mpu9250::readWHOAMI(){
-	
-	if(ioctl(i2c_fd, I2C_SLAVE, MPUADDR)<0){
-		std::cout<<"Failed to aquire bus access and/or talk to i2c slave."<<std::endl; 
-		exit(0);
-	}
-	wbuffer[0] = WHOAMI;
-	if(write(i2c_fd, wbuffer, 1)<1){
-		std::cout<<"Failed to write to the i2c bus."<<std::endl; 
-		return;
-	}
-/*
-	if(write(i2c_fd, wbuffer, 0)!= 0){
-		std::cout<<"Failed to write 'read' to the i2c bus."<<std::endl; 
-		return;
-	}*/
-//	readMPU(1);
+	readBytes(WHOAMI, 1);
 }
 
+void mpu9250::readCONFIG(){
+	readBytes(CONFIG,1);
+}
+
+
+void mpu9250::writeCONFIG(){
+/* Configuration takes in 1 byte. 
+7: resereved 
+6: fifo_mode -- 1) Additional writes will not be written to fifo when fifo is full
+			  2) Additional writes will be written to the fifo, replacing the oldest data
+5:3: EXT_SYNC_SET[2:0] external sync settings. //probably leave this as 0 
+2:0: DLPF, not sure what DLPF is 
+*/
+}
+void mpu9250::readGCONFIG(){
+	readBytes(GYRO_CONFIG,1);
+}
+
+void mpu9250::writeGCONFIG(unsigned char gconfig){
+
+/* Configuration takes in 1 byte. 
+7:5) self-test 
+4:3) gyro_frequence 00 = 250, 01 = 500, 10 = 1000, 11 = 2000
+	dps 
+   2) reserved 
+1:0) fchoice bypass 
+
+Generally speaking, we only want to modify values for bits 4:3.  */
+wbuffer[0] = GYRO_CONFIG; 
+wbuffer[1] = gconfig; 
+writeBytes(wbuffer,2); 
+}
 
 
 
@@ -100,9 +142,11 @@ mpu9250 mpu1;
 mpu1.readWHOAMI();
 std::cout<<WHOAMI<<std::endl;
 std::cout<<(int)mpu1.rbuffer[0]<<std::endl;
-mpu1.readBytes(0x3B, 2);
-std::cout<<(int)mpu1.rbuffer[1]<< mpu1.rbuffer[2]<<std::endl;
-
+mpu1.readCONFIG();
+std::cout<<(int)mpu1.rbuffer[0]<<std::endl;
+mpu1.writeGCONFIG((unsigned char)GCONFIG);
+mpu1.readGCONFIG();
+std::cout<<(int)mpu1.rbuffer[0]<<std::endl;
 
 return 0; 
 }
